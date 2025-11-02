cd5260e75be721097b5134769a0ab1e6bb2aa4e1
DAXTER1829 <diegoazarate110800@gmail.com>
2025-11-02 14:16:55 -0600

    editar perfil

--- AdApp-Frontend/AdApp/src/app/features/artist/artist-module.ts
+++ AdApp-Frontend/AdApp/src/app/features/artist/artist-module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ArtistRoutingModule } from './artist-routing-module';
 import { ArtistProfile } from './artist-profile/artist-profile';
@@ -13,6 +14,7 @@ import { ArtistEdit } from './artist-edit/artist-edit';
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     ArtistRoutingModule
   ]
 })
