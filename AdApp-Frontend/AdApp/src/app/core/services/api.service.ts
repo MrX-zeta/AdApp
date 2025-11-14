7724983bf01f4ea23aa44a08d4acfe5d8a69f241
MrX-zeta <brianluisruizperez@gmail.com>
2025-11-14 02:16:11 -0600

    correccion api.service

---
+++ AdApp-Frontend/AdApp/src/app/core/services/api.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private readonly API_URL = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) {}
+
+  private getHeaders(): HttpHeaders {
+    const token = localStorage.getItem('token');
+    return new HttpHeaders({
+      'Content-Type': 'application/json',
+      ...(token && { 'Authorization': `Bearer ${token}` })
+    });
+  }
+
+  get<T>(endpoint: string): Observable<T> {
+    return this.http.get<T>(`${this.API_URL}${endpoint}`, {
+      headers: this.getHeaders()
+    });
+  }
+
+  post<T>(endpoint: string, data: any): Observable<T> {
+    return this.http.post<T>(`${this.API_URL}${endpoint}`, data, {
+      headers: this.getHeaders()
+    });
+  }
+
+  put<T>(endpoint: string, data: any): Observable<T> {
+    return this.http.put<T>(`${this.API_URL}${endpoint}`, data, {
+      headers: this.getHeaders()
+    });
+  }
+
+  delete<T>(endpoint: string): Observable<T> {
+    return this.http.delete<T>(`${this.API_URL}${endpoint}`, {
+      headers: this.getHeaders()
+    });
+  }
+}
